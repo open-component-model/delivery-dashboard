bd211d2d15befb2a13cc12a5b96557fab4e8e555
Philipp Heil (zkdev) <dev@zeekay.dev>
2024-06-20 14:17:27 +0200

    Clean up eslint cfg
    - drop compat
    - tweak imports to eslint-flat-cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,67 +1,52 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
-import react from 'eslint-plugin-react'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
-
-export default [...fixupConfigRules(compat.extends(
-  'eslint:recommended',
-  'plugin:react/recommended',
-  'plugin:react-hooks/recommended',
-  'plugin:jsx-a11y/recommended',
-)), {
-  plugins: {
-    react: fixupPluginRules(react),
-    'react-hooks': fixupPluginRules(reactHooks),
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+export default [
+  js.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  reactRecommended,
+  {
+    rules: {
+      indent: ['error', 2, {
+        SwitchCase: 1,
+      }],
+      'linebreak-style': ['warn', 'unix'],
+      quotes: ['error', 'single'],
+      'no-extra-semi': ['error'],
+      semi: ['error', 'never'],
+      'react/prop-types': ['warn'],
+      'no-unused-vars': ['warn'],
+      'no-unreachable': ['warn'],
+      'eol-last': ['warn'],
+      'jsx-a11y/no-autofocus': ['off'],
     },
 
-    ecmaVersion: 13,
-    sourceType: 'module',
-
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    settings: {
+      react: {
+        version: '18.2.0',
       },
     },
-  },
 
-  settings: {
-    react: {
-      version: '18.2.0',
+    plugins: {
+      'react-hooks': reactHooks,
     },
-  },
 
-  rules: {
-    indent: ['error', 2, {
-      SwitchCase: 1,
-    }],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
 
-    'linebreak-style': ['warn', 'unix'],
-    quotes: ['error', 'single'],
-    'no-extra-semi': ['error'],
-    semi: ['error', 'never'],
-    'react/prop-types': ['warn'],
-    'no-unused-vars': ['warn'],
-    'no-unreachable': ['warn'],
-    'eol-last': ['warn'],
-    'jsx-a11y/no-autofocus': ['off'],
-  },
-}]
+      ecmaVersion: 13,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  }
+]
